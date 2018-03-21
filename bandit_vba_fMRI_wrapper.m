46a5875426e725ae5656c648503e2941be5b9239
Alex Dombrovski <dombrovskia@upmc.edu>
2018-03-21 11:30:24 -0400

    Disappointment version of evolution fx

--- bandit_vba_fMRI_wrapper.m
+++ bandit_vba_fMRI_wrapper.m
@@ -1,8 +1,8 @@
 %Handle dir paths
 dirs=dir('subjects');
 
-addpath('vba\')
-addpath('behav_scripts\')
+addpath('vba/')
+addpath('behav_scripts/')
 
 if ~exist('vba_output','dir')
     mkdir('vba_output');
@@ -16,11 +16,14 @@ load('fMRI_ids_to_run_vba_on.mat')
 %Set up input arguements
 graphics = 0;
 plot_subject=0;
-valence=1;
-fix_decay=0; %The logic is fixed
-utility=0;
 save_results=1;
-fix_all_params = 0;
+
+parameterization.valence=1;
+parameterization.fix_decay=0; %The logic is fixed
+parameterization.utility=0;
+parameterization.disappointment = 1;
+parameterization.regret = 0;
+parameterization.fix_all_params = 0;
 for i = 3:length(dirs)
     if dirs(i).bytes <=0 
         try
@@ -30,12 +33,12 @@ for i = 3:length(dirs)
             %Save all the ids processed
             idNumbers(i) = id;
             %[posterior,out,b] = bandit_vba(id,graphics,plot_subject,valence, decay,utility,save_results);
-            [posterior,out,b] = bandit_vba(id,graphics,plot_subject,valence, fix_decay,utility,save_results,fix_all_params);
+            [posterior,out,b] = bandit_vba(id,graphics,plot_subject,save_results,parameterization);
             
         catch exception
             
             %Record errors in logger
-            errorlog('bandit',b.id,exception)
+%             errorlog('bandit',b.id,exception)
         end
     end
 end
